b0c37597b1bddcddb5b95bce5fee1b36585d04dc
Jhon <jhon@evalucion.local>
2026-09-19 04:38:43 +0000

    Inicializa el proyecto: estructura base de Spring Boot

---
+++ src/main/java/com/evalucion/tvmazemiddleware/TvMazeMiddlewareApplication.java
@@ -0,0 +1,12 @@
+package com.evalucion.tvmazemiddleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvMazeMiddlewareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvMazeMiddlewareApplication.class, args);
+    }
+}
